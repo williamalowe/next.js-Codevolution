14f0a7c09285175f2d83308df07cf83e7f91c5a1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-25 14:36:15 +1000

    lesson: add title metadata lesson

--- 10-routing_metadata/app/blog/page.tsx
+++ 10-routing_metadata/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "blogs"
+  // title: {
+  //   absolute: "Blog"
+  // }
+}
 export default function Blog() {
   return <h1>My Blog</h1>
 }
