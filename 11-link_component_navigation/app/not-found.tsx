3b30b4ce596d33eb3a6badbeb53adc27b94de5df
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-25 14:44:38 +1000

    lesson: add link component lesson

---
+++ 11-link_component_navigation/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <>
+      <h3>Page not found</h3>
+      <p>Unable to find requested page</p>
+    </>
+  )
+}
---
+++ 11-link_component_navigation/app/products/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function ProductList() {
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <h1>Product List</h1>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>Product 3</h2>
+    </>
+  )
+}
