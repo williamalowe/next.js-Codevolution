3bbbb9ebc27722b6f00f3b9da5a8cd4f0dc7b616
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-26 13:59:46 +1000

    lesson: add conditional routes lesson

--- 14-parallel_routes/app/complex-dashboard/layout.tsx
+++ 14-parallel_routes/app/complex-dashboard/layout.tsx
@@ -1,24 +1,29 @@
-export default function DashboardLayout({ 
+export default function DashboardLayout({
   children,
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = false;
+  return isLoggedIn ? (
     <>
-    <div>{children}</div> {/* {Content from page.tsx} */}
-    <div style={{ display: "flex" }}>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div>{users}</div>
-        <div>{revenue}</div>
+      <div>{children}</div> {/* {Content from page.tsx} */}
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{users}</div>
+          <div>{revenue}</div>
+        </div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
-      <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
-    </div>
     </>
-  )
+  ) : (
+  login
+  );
 }
