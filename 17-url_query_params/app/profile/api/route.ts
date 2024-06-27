b8c4493f8a226dafbfe8895c1ed9cd67f2016065
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-27 12:52:43 +1000

    lesson: add headers in route handlers lesson

--- 17-url_query_params/app/profile/api/route.ts
+++ 17-url_query_params/app/profile/api/route.ts
@@ -1,3 +1,11 @@
-export async function GET() {
-  return new Response("Profile data");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  console.log(requestHeaders.get("Authorization"));
+  console.log(headerList.get("Authorization"));
+  return new Response("Profile API data");
 }
